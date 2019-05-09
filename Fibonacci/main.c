1cdab7d83f92aa519253ae179c060880eb19292f
Marcelo Arias <mail.marcelo.as@gmail.com>
2019-05-09 16:25:37 -0500

    Update main.c

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
-        printf("Fibonacci");
-        return 0;
+int main(){
+	printf("Secuencia de Fibonacci\n");
+	printf("----------------------\n");
+	printf("Término 1 = Número 1\n");
+	printf("Término 2 = Número 1\n");
+	printf("Término 3 = Número 2\n");
+	printf("Término 4 = Número 3\n");
+	printf("Término 5 = Número 5\n");
+	printf("Término 6 = Número 8\n");
 }
