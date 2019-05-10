592d29117124b0a2e69c643c32c19cd3c284d04f
Marcelo <mail.marcelo.as@gmail.com>
2019-05-10 00:05:19 -0500

    Update main.c

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -1,12 +1,46 @@
 #include<stdio.h>
 
 int main(){
+
+	int n;
+
 	printf("Secuencia de Fibonacci\n");
 	printf("----------------------\n");
-	printf("Término 1 = Número 1\n");
-	printf("Término 2 = Número 1\n");
-	printf("Término 3 = Número 2\n");
-	printf("Término 4 = Número 3\n");
-	printf("Término 5 = Número 5\n");
-	printf("Término 6 = Número 8\n");
+	printf("Term 1 = Number 0\n");
+	printf("Term 2 = Number 1\n");
+	printf("Term 3 = Number 1\n");
+	printf("Term 4 = Number 2\n");
+	printf("Term 5 = Number 3\n");
+	printf("Term 6 = Number 5\n");
+	printf("Term 7 = Number 8\n");
+	printf("----------------------\n");
+
+	// Solicitamos el término
+	printf("Input the term based in the Fibonacci Sequence\n");
+	scanf("%i\n", n);
+
+	int count = 4;
+	int zero = 0; //  Término 1
+	int once = 1; // Término 2
+	for (size_t i = 0; i < count; i++) {
+		// Si el término es 6, entonces lo repetiremos
+		//un total de 6-2 veces, es decir 4 veces:
+
+	}
+
+	// A es el número anterior a B
+	// B es el número presente
+	// C es el siguiente término
+	// A + B = C
+
+	// término N - 2 = veces N
+
+
+
+
+
+
+
+
+
 }
