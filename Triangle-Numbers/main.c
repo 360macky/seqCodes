814130628627eaad894ca477bda2eb1108176566
Marcelo <mail.marcelo.as@gmail.com>
2019-05-10 10:13:14 -0500

    Update main.c

--- Triangle-Numbers/main.c
+++ Triangle-Numbers/main.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int main(){
+
+  printf("Secuencia de Numeros Triángulos\n");
+	printf("---------------------------\n");
+	printf("Term 1 = Number 2\n");
+	printf("Term 2 = Number 3\n");
+	printf("Term 3 = Number 5\n");
+	printf("Term 4 = Number 7\n");
+	printf("Term 5 = Number 11\n");
+	printf("Term 6 = Number 13\n");
+	printf("Term 7 = Number 17\n");
+	printf("---------------------------\n");
+
+
+}
