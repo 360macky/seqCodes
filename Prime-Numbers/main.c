e5d485d8a7b89c1b7cd481b97ae28b1c13b6977d
Marcelo <mail.marcelo.as@gmail.com>
2019-07-18 11:55:11 -0500

    Update main.c

--- Prime-Numbers/main.c
+++ Prime-Numbers/main.c
@@ -13,51 +13,19 @@ int main(){
 	printf("Term 7 = Number 17\n");
 	printf("---------------------------\n");
 
-/*	Un bucle para encontrar los números primos	*/
 
-int n = 67;
-int i = 0;
-int limit = 4;
+  int resultArray[12];
 
+  for (size_t i = 2; i < 14; i++) {
+    int result = 14 % i;
 
+    int y = i - 2;
+    resultArray[y] = result;
+    // si el módulo (resultArray[y]) es 0, entonces es divisible.
+    // Esto solo se cumple en 2 y en 7
+  	printf("14 mod %i = %i\n", i, resultArray[y]);
 
-
-// Divide the number in all the numbers after itself
-
-
-/* Try */ /* Default case = 4 try */
-//for (i; i<limit; i++){
-	//if(n%2==0){
-
-	//}
-//}
-
-/*
-	NÚMERO PRIMO:
-	- Sólo Divisible entre UNO y por el mismo NÚMERO (All numbers)
-	- No es divisible por ningún otro número.
-	- No es UNO por definición
-
-	uniqueNumbers = {2,3,5,7,...}
-
-	If 'n' is the 'itself' number.
-	We try divide 'n' by 'uniqueNumbers', except if one number of 'uniqueNumbers'
-
-
-	TEST ALGORÍTMICO (dado n=67)
-
-	¿n%2 = 0? FALSE
-	¿n%3 = 0? FALSE
-	¿n%5 = 0? FALSE
-	¿n%7 = 0? FALSE
-
-	If everything before = FALSE {
-		'n' = 'primeNumber'
-	}
-
-
-
-*/
+  }
 
 
 }
