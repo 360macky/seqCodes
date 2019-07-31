089b7b81f27e73f5416720aaac30d41446f5678e
Marcelo <mail.marcelo.as@gmail.com>
2019-07-31 10:27:25 -0500

    Update main.c

--- Prime-Numbers/main.c
+++ Prime-Numbers/main.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
 
-
 int main(){
 
   printf("Secuencia de Numeros Primos\n");
-	printf("---------------------------\n");
-	printf("Term 1 = Number 2\n");
-	printf("Term 2 = Number 3\n");
-	printf("Term 3 = Number 5\n");
-	printf("Term 4 = Number 7\n");
-	printf("Term 5 = Number 11\n");
-	printf("Term 6 = Number 13\n");
-	printf("Term 7 = Number 17\n");
 	printf("---------------------------\n");
 
   int n;
@@ -20,7 +11,7 @@ int main(){
   int start;
 
   printf("Ingresa el numero limite\n");
-  n = 10;
+  scanf("%d\n", &n);
 
   // Define an array of numbers since 2 (bcoz 1 not for be a prime Number), to N number.
   for (start=2; start<=n; start++) {
@@ -37,7 +28,7 @@ int main(){
 
           // Start to evaluate each number for module
           int x = start % module;
-          printf("%d modulo %d = %d\n", start, module, x);
+
           if (start % module == 0) {
             iterator++;
           }
