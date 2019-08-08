2d71723c5f72924002badea641b28517221033c5
Marcelo <mail.marcelo.as@gmail.com>
2019-08-07 22:07:26 -0500

    Improved

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -4,26 +4,22 @@ int main(){
 
 	printf("Secuencia de Fibonacci\n");
 	printf("----------------------\n");
-	printf("Term 1 = Number 0\n");
-	printf("Term 2 = Number 1\n");
-	printf("Term 3 = Number 1\n");
-	printf("Term 4 = Number 2\n");
-	printf("Term 5 = Number 3\n");
-	printf("Term 6 = Number 5\n");
-	printf("Term 7 = Number 8\n");
-	printf("----------------------\n");
 
 	// Solicitamos el término
 	printf("Input the term based in the Fibonacci Sequence\n");
 	int n;
-	scanf("%d\n", &n);
+	scanf("%d", &n);
+
+	// Restamos 2 números, a causa del término 1, término 2 y término 3
+	n = n - 3;
+	printf("----------------------\n");
 
 	// Variable
-	//int n = 10;
-
 
 	int a = 1;
 	int b = 2;
+
+	printf("0\n%i\n%i\n", a, b);
 
 	for (size_t i = 0; i < n; i++) {
 
