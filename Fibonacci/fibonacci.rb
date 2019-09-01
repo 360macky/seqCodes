230565bfc577a7384ff2df236af9db3938000aa3
Marcelo <mail.marcelo.as@gmail.com>
2019-08-31 22:51:10 -0500

    Create fibonacci.rb

---
+++ Fibonacci/fibonacci.rb
@@ -0,0 +1,22 @@
+# Fibonnaci Sequence on Ruby
+
+# git-repository: https://github.com/360macky/seqCodes
+# author: Marcelo A.S.
+
+# Solicitamos el término
+puts "Fibonacci"
+puts "Ingresa el número de términos de la secuencia de Fibonacci"
+n = gets
+
+# Restamos 4 numeros
+n = n
+
+# Proceso
+a = 1
+b = 2
+
+puts "0"
+puts a
+puts a
+puts b
+
