bad78323afc0098a05f89016fa72bbbeaa43fe9c
Marcelo <mail.marcelo.as@gmail.com>
2019-08-31 23:03:41 -0500

    Complete!

--- Fibonacci/fibonacci.rb
+++ Fibonacci/fibonacci.rb
@@ -6,17 +6,30 @@
 # Solicitamos el término
 puts "Fibonacci"
 puts "Ingresa el número de términos de la secuencia de Fibonacci"
-n = gets
+n = gets.chomp.to_i
 
 # Restamos 4 numeros
-n = n
+n = n - 4
 
 # Proceso
 a = 1
 b = 2
 
-puts "0"
+puts "++++++++++"
+
+puts 0
 puts a
 puts a
 puts b
 
+i = 0
+
+while i < n
+    sum = a + b
+
+    puts sum
+    a = b
+    b = sum
+    i+=1
+
+end
