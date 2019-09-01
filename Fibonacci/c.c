5065deeb25538f200336eb66b3e9b27e10e62727
Marcelo <mail.marcelo.as@gmail.com>
2019-08-31 19:33:42 -0500

    New name

---
+++ Fibonacci/c.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+	// Título
+	printf("+-+-+-+-+-+-+-+-+-+\n");
+	printf("|F|i|b|o|n|a|c|c|i|\n");
+	printf("+-+-+-+-+-+-+-+-+-+\n");
+
+	// Solicitamos el término
+	printf("Ingresa el número de términos de la secuencia de Fibonacci\n");
+	int n;
+	scanf("%d", &n);
+
+	// Restamos 2 números, a causa del término 1, término 2 y término 3
+	n = n - 4;
+
+	// Un poco de diseño
+	printf("+-+-+-+-+-+-+-+-+-+\n");
+
+	// Variable
+	int a = 1;
+	int b = 2;
+
+	printf("0\n%i\n%i\n%i\n", a, a, b);
+
+	for (size_t i = 0; i < n; i++) {
+
+		// Obtenemos el número mayor
+		int sum = a + b;
+
+		// Imprimimos la suma
+		printf("%i\n", sum);
+
+		// Switcheamos las variables
+		a = b;
+		b = sum;
+	}
+}
